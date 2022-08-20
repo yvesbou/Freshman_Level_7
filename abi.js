3502263a0ffa1598b38e0bf6efa7bc36c3d9ec44
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-08-20 06:56:50 +0200

    finished

---
+++ abi.js
@@ -0,0 +1,28 @@
+const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mood",
+				"type": "string"
+			}
+		],
+		"name": "setMood",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getMood",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
